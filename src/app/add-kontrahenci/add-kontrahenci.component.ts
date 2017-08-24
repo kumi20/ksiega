7ccd604ca2244f5c9e097c0c8d382f8e1a09b6f9
kumi20 <j.kumiega@icloud.com>
2017-08-24 22:22:54 +0200

    dodanie komponentu podatku dochodowego i komponentu dodawania podatku

--- src/app/add-kontrahenci/add-kontrahenci.component.ts
+++ src/app/add-kontrahenci/add-kontrahenci.component.ts
@@ -11,13 +11,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class AddKontrahenciComponent implements OnInit {
   
   kontrahenci;
-  idKontrahenta;
   
   constructor(private ksiegaService: KsiegaService, private route: ActivatedRoute, private _route: Router) { }
 
   ngOnInit() {
-    
-      this.route.params.subscribe(params => this.idKontrahenta = params['id']);
       this.kontrahenci = new FormGroup(
         {
             nip: new FormControl("", Validators.required),
@@ -33,43 +30,6 @@ export class AddKontrahenciComponent implements OnInit {
             dostawca: new FormControl(),
             odbiorca: new FormControl()
         }
-      )
-    
-      if (this.idKontrahenta != null){
-          this.ksiegaService.getDetKontrahenta(this.idKontrahenta).subscribe(
-              response => {
-                  this.kontrahenci.controls['nip'].setValue(response[0].NIP);
-                  this.kontrahenci.controls['nazwa'].setValue(response[0].Name);
-                  this.kontrahenci.controls['ulica'].setValue(response[0].Street);
-                  this.kontrahenci.controls['kodPocztowy'].setValue(response[0].Postcode);
-                  this.kontrahenci.controls['miejscowosc'].setValue(response[0].miejscowosc);
-                  this.kontrahenci.controls['osobaKontaktowa'].setValue(response[0].Person_contact);
-                  this.kontrahenci.controls['telefon'].setValue(response[0].telephone);
-                  this.kontrahenci.controls['fax'].setValue(response[0].fax);
-                  this.kontrahenci.controls['email'].setValue(response[0].email);
-                  this.kontrahenci.controls['www'].setValue(response[0].www);
-                  this.kontrahenci.controls['dostawca'].setValue(response[0].dostawca=="0"?false:true);
-                  this.kontrahenci.controls['odbiorca'].setValue(response[0].odbiorca=="0"?false:true);
-                
-              }
-          )
-      }
-    else this.idKontrahenta = 0;
-  }
-  
-  powrot(){
-      history.back();
-  }
-  
-  zapisz(dateControl){
-      this.ksiegaService.saveKontrahent(dateControl, this.idKontrahenta).subscribe(
-          response => history.back()
-      )
-  }
-  
-  usun(){
-      this.ksiegaService.deleteKOntrahent(this.idKontrahenta).subscribe(
-          response => history.back()
       )
   }
 
